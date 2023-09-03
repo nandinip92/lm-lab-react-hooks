ad5243ec9e3c1d6d8839c9c93c18f0ce562badd6
nandinip92 <nandini.p8892@gmail.com>
2023-09-03 07:02:08 +0100

    feat: Implementing 'useRef' to count_render and focus

--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from "react";
 
 export const CountRenders = () => {
-	const [value, setValue] = useState('');
-	const count = 1;
+  const [value, setValue] = useState("");
+  const count = 1;
+  const renderCount = useRef(0);
 
-	return (
-		<>
-			<h2>useRef</h2>
+  useEffect(() => {
+    renderCount.current = renderCount.current + count;
+    // console.log("render--->", renderCount);
+  });
 
-			<input
-				value={value}
-				type='text'
-				onChange={(e) => setValue(e.target.value)}
-			/>
+  return (
+    <>
+      <h2>useRef</h2>
 
-			<p>{value}</p>
-			<p>I have rendered {count} times</p>
-		</>
-	);
+      <input
+        value={value}
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+      />
+
+      <p>{value}</p>
+      <p>I have rendered {renderCount.current} times</p>
+    </>
+  );
 };
--- src/components/hooks/use_ref/focus.tsx
+++ src/components/hooks/use_ref/focus.tsx
@@ -1,17 +1,17 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
 export const Focus: React.FC = () => {
-
   const inputRef = useRef<HTMLInputElement>(null);
 
+  //console.log("inputRef-->", inputRef);
   const handleClick = () => {
     inputRef.current?.focus();
   };
 
   return (
     <div>
-      <input type="text" />
+      <input ref={inputRef} type="text" />
       <button onClick={handleClick}>Focus Input</button>
     </div>
   );
-}
+};
