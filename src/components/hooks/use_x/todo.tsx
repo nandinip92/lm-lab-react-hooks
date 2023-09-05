af83e623974d375f7006da0ba8880a0b4aaaaeee
nandinip92 <nandini.p8892@gmail.com>
2023-09-05 20:02:13 +0100

    fix: removing unnecessary code

--- src/components/hooks/use_x/todo.tsx
+++ src/components/hooks/use_x/todo.tsx
@@ -1,13 +1,5 @@
 import useTodo from "./use_todo";
 
-/** This is the response that TypiCode gives for the /todos/ endpoint */
-interface TodoResponse {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
 export const Todo = () => {
   const { data, isFetching } = useTodo(
     "https://jsonplaceholder.typicode.com/todos/1"
